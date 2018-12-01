06c58329b563c79d71d448ed24f981d0efc92cee
Steven Persia <persia.steven8@gmail.com>
2018-12-01 10:29:25 +0100

    Add avatar and interests in User model and change some var to const

--- index.js
+++ index.js
@@ -1,68 +1,67 @@
 // IMPORTS
-const express = require("express");
+const express = require('express');
 const app = express();
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-app.use(bodyParser.json({
-    limit: "10mb"
-}));
-const cors = require("cors");
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+app.use(
+	bodyParser.json({
+		limit: '10mb'
+	})
+);
+const cors = require('cors');
 
 // `Cross-Origin Resource Sharing` est un mechanisme permettant d'autoriser les
 // requêtes provenant d'un nom de domaine different Ici, nous autorisons l'API
 // à repondre aux requêtes AJAX venant d'autres serveurs
 
-app.use("/api", cors());
+app.use('/api', cors());
 
 // CONNEXION AU SERVEUR
-mongoose.connect(process.env.URI || "mongodb://localhost:27017/human_challenge", {
-        useNewUrlParser: true
-    },
-    function (err) {
-        if (err) console.error("Could not connect to mongodb.");
-    }
-
+mongoose.connect(
+	process.env.URI || 'mongodb://localhost:27017/human_challenge',
+	{
+		useNewUrlParser: true
+	},
+	function(err) {
+		if (err) console.error('Could not connect to mongodb.');
+	}
 );
 
 // Import des Models
-
-var User = require("./models/User");
-var Challenge = require("./models/Challenge");
-var Badges = require("./models/Badges");
-var Category = require("./models/Category");
-var Prerequisite = require("./models/Prerequisite");
+const User = require('./models/User');
+const Challenge = require('./models/Challenge');
+const Badges = require('./models/Badges');
+const Category = require('./models/Category');
+const Prerequisite = require('./models/Prerequisite');
 
 // Import des Routes
 // Les routes sont séparées dans plusieurs fichiers
-// var coreRoutes = require("./routes/core.js");
-// var userRoutes = require("./routes/user.js");
-var challengeRoutes = require("./routes/challenge.js");
+// const coreRoutes = require("./routes/core.js");
+// const userRoutes = require("./routes/user.js");
+const challengeRoutes = require('./routes/challenge.js');
 
 // Les routes relatives aux utilisateurs auront pour prefix d'URL `/user`
 // app.use("/api", coreRoutes);
 // app.use("/api/user", userRoutes);
-app.use("/api/challenge", challengeRoutes);
-
-
+app.use('/api/challenge', challengeRoutes);
 
 // Première page
 
-app.get("/", function (req, res) {
-    res.send("Welcome to Human Challenge API.");
-    console.log("hello");
-
+app.get('/', function(req, res) {
+	res.send('Welcome to Human Challenge API.');
+	console.log('hello');
 });
 
 // Toutes les méthodes HTTP (GET, POST, etc.) des pages non trouvées afficheront
 // une erreur 404
-app.all("*", function (req, res) {
-    res.status(404).json({
-        error: "Not Found"
-    });
+app.all('*', function(req, res) {
+	res.status(404).json({
+		error: 'Not Found'
+	});
 });
 
 // Initialisation du serveur
 
-app.listen(process.env.PORT || 3000, function () {
-    console.log("Serveur initialisé");
+app.listen(process.env.PORT || 3000, function() {
+	console.log('Serveur initialisé');
 });
--- models/User.js
+++ models/User.js
@@ -1,54 +1,56 @@
 // IMPORTS
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 // USER MODEL
 
 const UserSchema = new mongoose.Schema({
-    account: {
-        username: String,
-        email: String,
-        biography: String,
-        phoneNumber: Number
-    },
-    organizer: {
-        type: String /* Association, Particulier, Institution... */
-    },
-    security: {
-        token: String,
-        hash: String,
-        salt: String,
-        pepper: String,
-        smsCode: String
-    },
-    challenges: [{
-        id: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Challenge"
-        },
-        status: String
-    }],
-    location: {
-        adressLine1: String,
-        adressLine2: String,
-        city: String,
-        zipCode: Number,
-        latitude: Number,
-        longitude: Number
-    },
-    badges: Array,
+	account: {
+		username: String,
+		email: String,
+		biography: String,
+		phoneNumber: Number,
+		avatar: String,
+		interests: Array
+	},
+	organizer: {
+		type: String /* Association, Particulier, Institution... */
+	},
+	security: {
+		token: String,
+		hash: String,
+		salt: String,
+		pepper: String,
+		smsCode: String
+	},
+	challenges: [
+		{
+			id: {
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'Challenge'
+			},
+			status: String
+		}
+	],
+	location: {
+		adressLine1: String,
+		adressLine2: String,
+		city: String,
+		zipCode: Number,
+		latitude: Number,
+		longitude: Number
+	},
+	badges: Array,
 
-    // STP Farid comment on peut assigner à une categorie un nombre dans la base de donnée, merci.
+	// STP Farid comment on peut assigner à une categorie un nombre dans la base de donnée, merci.
 
-    // [{
-    //     type: mongoose.Schema.Types.ObjectId,
-    //     ref: "Category"
-    // }],
-    rpgCharacter: {
-        class: String,
-        experience: Number
-    }
-
-
+	// [{
+	//     type: mongoose.Schema.Types.ObjectId,
+	//     ref: "Category"
+	// }],
+	rpgCharacter: {
+		class: String,
+		experience: Number
+	}
 });
 
-module.exports = mongoose.model("User", UserSchema, "users");
+module.exports = mongoose.model('User', UserSchema, 'users');
