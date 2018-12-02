09b1f97c3f79dbe3cb3946d9256056b5a459f4a0
Steven Persia <persia.steven8@gmail.com>
2018-12-02 16:15:23 +0100

    Change type of Phone Number / Finished Login & SignUp routes / Create Profile et Settings routes files

--- routes/login.js
+++ routes/login.js
@@ -2,25 +2,32 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/User.js');
 
+const SHA256 = require('crypto-js/sha256');
+const encBase64 = require('crypto-js/enc-base64');
+
 router.post('/', function(req, res, next) {
-	User.findOne({ phone: req.body.phone }).exec(function(err, user) {
-		if (err) return next(err.message);
-		if (user) {
-			if (
-				SHA256(req.body.password + user.salt).toString(encBase64) === user.hash
-			) {
-				return res.json({
-					_id: user._id,
-					token: user.token,
-					account: user.account
-				});
+	User.findOne({ 'account.phoneNumber': req.body.account.phoneNumber }).exec(
+		function(err, user) {
+			if (err) return next(err.message);
+			if (user) {
+				if (
+					SHA256(req.body.account.password + user.security.salt).toString(
+						encBase64
+					) === user.security.hash
+				) {
+					return res.json({
+						_id: user._id,
+						token: user.token,
+						account: user.account
+					});
+				} else {
+					return res.status(401).json({ error: 'Unauthorized' });
+				}
 			} else {
-				return res.status(401).json({ error: 'Unauthorized' });
+				return next('User not found');
 			}
-		} else {
-			return next('User not found');
 		}
-	});
+	);
 });
 
 module.exports = router;
--- routes/signup.js
+++ routes/signup.js
@@ -10,26 +10,33 @@ const encBase64 = require('crypto-js/enc-base64');
 router.post('/', function(req, res, next) {
 	const token = uid2(64);
 	const salt = uid2(64);
-	const hash = SHA256(req.body.password + salt).toString(encBase64);
+	const hash = SHA256(req.body.account.password + salt).toString(encBase64);
 
 	// Rendre username, password, phone obligatoire
 	const user = new User({
 		account: {
-			username: req.body.username,
-			email: req.body.email,
-			phoneNumber: req.body.phone,
-			avatar: req.body.avatar,
-			interests: req.body.interests
+			username: req.body.account.username,
+			email: req.body.account.email,
+			phoneNumber: req.body.account.phoneNumber,
+			avatar: req.body.account.avatar,
+			interests: req.body.account.interests
 		},
 		security: {
 			token: token,
 			hash: hash,
 			salt: salt,
 			pepper: 'Private Joke',
-			smsCode: req.body.smsCode
+			smsCode: req.body.security.smsCode
 		}
 	});
 	user.save(function(err) {
+		/* If phone number already exist */
+		if (err.code === 11000) {
+			return res.status(400).json({
+				error: 'Phone number must be unique'
+			});
+		}
+
 		if (err) {
 			return next(err.message);
 		} else {
