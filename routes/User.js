e96ba4dc15c0ddf531dcee64d4e999169b2f200d
delphinedelf <def.messageriefr@gmx.fr>
2018-12-05 14:42:00 +0100

    rajout route user

---
+++ routes/User.js
@@ -0,0 +1,66 @@
+// IMPORTS
+const mongoose = require('mongoose');
+
+// USER MODEL
+
+const UserSchema = new mongoose.Schema({
+	account: {
+		username: String,
+		email: String,
+		biography: String,
+		phoneNumber: {
+			type: String,
+			unique: true
+		},
+		avatar: String,
+		interests: Array
+	},
+	organizer: {
+		type: String /* Association, Particulier, Institution... */
+	},
+	security: {
+		token: String,
+		hash: String,
+		salt: String,
+		pepper: String,
+		smsCode: String
+	},
+	challenges: {
+		player: [{
+			id: {
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'Challenge'
+			},
+			status: String
+		}],
+		manager: [{
+			id: {
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'Challenge'
+			},
+			status: String
+		}]
+	},
+	location: {
+		adressLine1: String,
+		adressLine2: String,
+		city: String,
+		zipCode: Number,
+		latitude: Number,
+		longitude: Number
+	},
+	badges: Array,
+
+	// STP Farid comment on peut assigner à une categorie un nombre dans la base de donnée, merci.
+
+	// [{
+	//     type: mongoose.Schema.Types.ObjectId,
+	//     ref: "Category"
+	// }],
+	rpgCharacter: {
+		class: String,
+		experience: Number
+	}
+});
+
+module.exports = mongoose.model('User', UserSchema, 'users');
