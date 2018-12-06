04021bbb3a35247a6911feaa29b0386a09b20fec
delphinedelf <def.messageriefr@gmx.fr>
2018-12-06 14:54:07 +0100

    remplacé user.js

--- routes/User.js
+++ routes/User.js
@@ -1,66 +1,111 @@
-// IMPORTS
-const mongoose = require('mongoose');
+var express = require("express");
+var router = express.Router();
+var User = require("../models/User.js");
+var Challenge = require("../models/Challenge.js");
+var isAuthenticated = require("../middlewares/isAuthenticated");
+// var uploadPictures = require("../middlewares/uploadPictures");
+var ObjectId = require("mongoose").Types.ObjectId;
 
-// USER MODEL
+// L'authentification est obligatoire pour cette route
 
-const UserSchema = new mongoose.Schema({
-	account: {
-		username: String,
-		email: String,
-		biography: String,
-		phoneNumber: {
-			type: String,
-			unique: true
-		},
-		avatar: String,
-		interests: Array
-	},
-	organizer: {
-		type: String /* Association, Particulier, Institution... */
-	},
-	security: {
-		token: String,
-		hash: String,
-		salt: String,
-		pepper: String,
-		smsCode: String
-	},
-	challenges: {
-		player: [{
-			id: {
-				type: mongoose.Schema.Types.ObjectId,
-				ref: 'Challenge'
-			},
-			status: String
-		}],
-		manager: [{
-			id: {
-				type: mongoose.Schema.Types.ObjectId,
-				ref: 'Challenge'
-			},
-			status: String
-		}]
-	},
-	location: {
-		adressLine1: String,
-		adressLine2: String,
-		city: String,
-		zipCode: Number,
-		latitude: Number,
-		longitude: Number
-	},
-	badges: Array,
+// PARTICIPER AU CHALLENGE
+router.put("/participate/:id", isAuthenticated, function(req, res, next) {
+  Challenge.findOne({
+    _id: ObjectId(req.params.id)
+  })
+    .exec()
+    .then(function(challenge) {
+      if (!challenge) {
+        res.status(404);
+        return next("Challenge not found");
+      }
+      // on verifie si utilisateur n'existe pas déjà ds le tableau des challengers pour faire le challenge
+      let challengerExists = false;
+      for (let i = 0; i < challenge.challengers.length; i++) {
+        // attention id sont des objets, donc les convertir en string
+        if (challenge.challengers[i].toString() === req.user._id.toString()) {
+          challengerExists = true;
+        }
+      }
 
-	// STP Farid comment on peut assigner à une categorie un nombre dans la base de donnée, merci.
+      // si l'utilisateur a créé le challenge, il ne peut pas s'inscrire
+      if (req.user._id.toString() === challenge.owner._id.toString()) {
+        return res.send(
+          "vous avez créé le challenge pas besoin de vous inscrire"
+        );
+      }
+      // si l'utilisateur n'est pas déjà inscrit au challenge, on l'inscrit
+      if (challengerExists === false) {
+        challenge.challengers.push(req.user._id);
+        challenge.save(function(err) {
+          if (err) {
+            return next(err.message);
+          } else {
+            // associé au modele user (clé manager)
+            req.user.challenges.player.push(challenge._id); // ou challenge
+            req.user.save();
 
-	// [{
-	//     type: mongoose.Schema.Types.ObjectId,
-	//     ref: "Category"
-	// }],
-	rpgCharacter: {
-		class: String,
-		experience: Number
-	}
+            return res.json({ user: req.user, challenge: challenge });
+          }
+        });
+      }
+      if (challengerExists === true) {
+        return res.send("déjà inscrit au challenge");
+      }
+    })
+    .catch(function(err) {
+      res.status(400);
+      return next(err.message);
+    });
 });
 
-module.exports = mongoose.model('User', UserSchema, 'users');
+// SUPPRESSION DE SA PARTICIPATION AU CHALLENGE
+router.delete("/remove/:id", isAuthenticated, function(req, res, next) {
+  Challenge.findOne({
+    _id: ObjectId(req.params.id)
+  })
+    .exec()
+    .then(function(challenge) {
+      if (!challenge) {
+        res.status(404);
+        return next("Challenge pas trouvé");
+      }
+
+      // si id de l'user est ds le tableau challenge on la supprime
+      let place = challenge.challengers.indexOf(req.user._id);
+      if (challenge.challengers.indexOf(req.user._id) === -1) {
+        res.status(404);
+        return next("Joueur pas trouvé");
+      }
+
+      challenge.challengers.splice(place, 1);
+
+      // on parcours le tableau player de req user pour supprimer le challenge
+      for (let i = 0; i < req.user.challenges.player.length; i++) {
+        if (
+          req.user.challenges.player[i]._id.toString() ===
+          challenge._id.toString()
+        ) {
+          req.user.challenges.player.splice(i, 1);
+        }
+      }
+
+      challenge.save(function(err) {
+        if (err) {
+          return next(err.message);
+        } else {
+          // associé au modele user (clé manager)
+
+          req.user.save();
+
+          return res.json({ message: "supprimé" });
+        }
+      });
+    })
+    .catch(function(err) {
+      res.status(400);
+      return next(err.message);
+    });
+});
+
+module.exports = router;
