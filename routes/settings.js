329c98ae4a49a278c6c85d82246ea6f399f07f23
Steven Persia <persia.steven8@gmail.com>
2018-12-12 15:12:44 +0100

    Change Update route

--- routes/settings.js
+++ routes/settings.js
@@ -1,104 +1,109 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
-var User = require("../models/User.js");
-var Challenge = require("../models/Challenge.js");
-var isAuthenticated = require("../middlewares/isAuthenticated");
-var uploadPictures = require("../middlewares/uploadPictures");
-var ObjectId = require("mongoose").Types.ObjectId;
+var User = require('../models/User.js');
+var Challenge = require('../models/Challenge.js');
+var isAuthenticated = require('../middlewares/isAuthenticated');
+var uploadPictures = require('../middlewares/uploadPictures');
+var ObjectId = require('mongoose').Types.ObjectId;
 
 // Password
-const SHA256 = require("crypto-js/sha256");
-const encBase64 = require("crypto-js/enc-base64");
+const SHA256 = require('crypto-js/sha256');
+const encBase64 = require('crypto-js/enc-base64');
 
 // Show account details
-router.get("/:id", isAuthenticated, function (req, res, next) {
-  User.findById(req.params.id)
-    .exec()
-    .then(function (user) {
-      if (!user) {
-        res.status(404);
-        return next("User not found");
-      }
+router.get('/:id', isAuthenticated, function(req, res, next) {
+	User.findById(req.params.id)
+		.exec()
+		.then(function(user) {
+			if (!user) {
+				res.status(404);
+				return next('User not found');
+			}
 
-      return res.json({
-        user
-      });
-    })
-    .catch(function (err) {
-      res.status(400);
-      return next(err.message);
-    });
+			return res.json({
+				user
+			});
+		})
+		.catch(function(err) {
+			res.status(400);
+			return next(err.message);
+		});
 });
 
 // Update account
-router.put("/update/:id", isAuthenticated, uploadPictures, function (
-  req,
-  res,
-  next
+router.put('/update/:id', isAuthenticated, uploadPictures, function(
+	req,
+	res,
+	next
 ) {
-  User.findById(req.params.id, function (err, user) {
-    if (err) {
-      res.status(400);
-      return next("An error occured");
-    } else {
-      if (req.body.security.password) {
-        const hash = SHA256(
-          req.body.security.password + user.security.salt
-        ).toString(encBase64);
-        user.security.hash = hash;
-        req.body.security && Object.assign(user.security, req.body.security);
-      }
-      req.body.account && Object.assign(user.account, req.body.account);
+	User.findByIdAndUpdate(req.params.id, function(err, user) {
+		if (err) {
+			res.status(400);
+			return next('An error occured');
+		} else {
+			if (req.body.security.password) {
+				const hash = SHA256(
+					req.body.security.password + user.security.salt
+				).toString(encBase64);
+				user.security.hash = hash;
+				req.body.security && Object.assign(user.security, req.body.security);
+			}
+			req.body.account && Object.assign(user.account, req.body.account);
 
-      if (req.pictures) {
-        user.account.avatar = req.pictures;
-      }
+			if (req.pictures) {
+				user.account.avatar = req.pictures;
+			}
 
-      user.save(e => {console.log(e, e.message)});
-      return res.json(user);
-    }
-  });
+			user.save(e => {
+				console.log(e, e.message);
+			});
+			return res.json(user);
+		}
+	});
 });
 
 // Delete account
-router.delete("/remove/:id", isAuthenticated, function (req, res, next) {
-  User.remove({
-      _id: ObjectId(req.params.id)
-    },
-    function (err, user) {
-      if (err) {
-        return next(err.message);
-      }
-      if (!user) {
-        res.status(404);
-        return next("Nothing to delete");
-      }
-      Challenge.remove({
-          $and: [{
-              owner: ObjectId(req.params.id)
-            },
-            {
-              "date.endDate": {
-                $gte: new Date()
-              }
-            }
-          ]
-        },
-        function (err, challengeFound) {
-          if (err) {
-            return next(err.message);
-          }
-          if (!challengeFound) {
-            res.status(404);
-            return next("No challenge to delete");
-          }
-        }
-      );
-      return res.json({
-        message: "Account and challenges of account (date not passed) deleted"
-      });
-    }
-  );
+router.delete('/remove/:id', isAuthenticated, function(req, res, next) {
+	User.remove(
+		{
+			_id: ObjectId(req.params.id)
+		},
+		function(err, user) {
+			if (err) {
+				return next(err.message);
+			}
+			if (!user) {
+				res.status(404);
+				return next('Nothing to delete');
+			}
+			Challenge.remove(
+				{
+					$and: [
+						{
+							owner: ObjectId(req.params.id)
+						},
+						{
+							'date.endDate': {
+								$gte: new Date()
+							}
+						}
+					]
+				},
+				function(err, challengeFound) {
+					if (err) {
+						return next(err.message);
+					}
+					if (!challengeFound) {
+						res.status(404);
+						return next('No challenge to delete');
+					}
+				}
+			);
+			return res.json({
+				message: 'Account and challenges of account (date not passed) deleted'
+			});
+		}
+	);
 });
 
 module.exports = router;
